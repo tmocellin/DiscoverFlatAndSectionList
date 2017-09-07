38f21b90731aa799999aad73bacdd30c4a520238
Tybi <developer@dwastudio.fr>
2017-09-07 16:03:37 -0400

    add userRow

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { FlatList, Text } from "react-native";
+import { FlatList, Text, View } from "react-native";
+import UserRow from "./UserRow";
 
-const _renderItem = ({ item }) => <Text>{item.email}</Text>;
+const _renderItem = ({ item }) => (
+  <UserRow
+    name={item.name.last}
+    fistName={item.name.first}
+    picture={item.picture.thumbnail}
+    email={item.email}
+  />
+);
+const _renderSeparator = () => (
+  <View style={{ height: 1, backgroundColor: "grey", marginLeft: 80 }} />
+);
 
 export default (UserList = props => (
   <FlatList
     data={props.data}
     renderItem={_renderItem}
     keyExtractor={item => item.email}
+    ItemSeparatorComponent={_renderSeparator}
   />
 ));
